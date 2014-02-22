7a7281066ce5f346e0a68f0dd8b6bffea65a4480
BugYang <yzy-sjtu@qq.com>
2014-02-22 15:16:04 +0800

    add iterative solution for preorder traversal

--- PreOrder.java
+++ PreOrder.java
@@ -21,4 +21,23 @@ public class Solution {
         }
         return result;
     }
+
+    public ArrayList<Integer> preorderTraversal_Iter(TreeNode root) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        if (root != null) {
+            stack.push(root);
+        }
+        while (stack.empty() == false) {
+            TreeNode current = stack.pop();
+            result.add(current.val);
+            if (current.right != null) {
+                stack.push(current.right);
+            }
+            if (current.left != null) {
+                stack.push(current.left);
+            }
+        }
+        return result;
+    }
 }
