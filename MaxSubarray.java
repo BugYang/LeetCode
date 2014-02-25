4a3349bd7511f3e263bd3410b3f5470a6b935fe7
Yang Zhiyuan <sjtu.yzy@gmail.com>
2014-02-25 10:30:39 +0800

    refact code for max subarray

--- MaxSubarray.java
+++ MaxSubarray.java
@@ -1,11 +1,7 @@
 public class Solution {
     public int maxSubArray_On(int[] A) {
-        if (A.length == 1) {
-            return A[0];
-        }
-
-        int T = 0, minT = 0, max = A[0];
-        for (int i = 0; i < A.length; i++) {
+        int T = A[0], minT = Math.min(0, A[0]), max = A[0];
+        for (int i = 1; i < A.length; i++) {
             T += A[i];
             max = T-minT > max ? T-minT : max;
             minT = T < minT ? T : minT;
