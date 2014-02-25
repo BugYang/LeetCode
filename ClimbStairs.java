2365328a5f922c4ec358f3a2996e1555f6affffb
Yang Zhiyuan <sjtu.yzy@gmail.com>
2014-02-25 08:14:48 +0800

    add solution for climbing stairs

---
+++ ClimbStairs.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public int climbStairs(int n) {
+        int a = 1, b = 1, result = 1;
+        for (int i = 2; i <= n; i++) {
+            result = a+b;
+            a = b;
+            b = result;
+        }
+        return result;
+    }
+}
