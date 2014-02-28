3dd2215af85200bd6a1f4f39c10eb41ce2675023
Yang Zhiyuan <sjtu.yzy@gmail.com>
2014-02-28 15:25:01 +0800

    add non-recursive solution for symmetric tree

--- SymmetricTree.java
+++ SymmetricTree.java
@@ -8,7 +8,7 @@
  * }
  *         */
 public class Solution {
-    public boolean isSymmetric(TreeNode root) {
+    public boolean isSymmetric_recur(TreeNode root) {
         return root == null ? true : cmpNode(root.left, root.right);
     }
 
@@ -19,5 +19,37 @@ public class Solution {
             return node1.val == node2.val 
                 && cmpNode(node1.left, node2.right) && cmpNode(node1.right, node2.left);
         }
+    }
+
+    public boolean isSymmetric_iter(TreeNode root) {
+        Stack<TreeNode> lstack = new Stack<TreeNode>(), rstack = new Stack<TreeNode>();
+        if (root && root.left) {
+            lstack.push(root.left);
+        }
+        if (root && root.right) {
+            rstack.push(root.right);
+        }
+        while (lstack.size() == rstack.size()) {
+            if (lstack.empty() && rstack.empty()) {
+                return true;
+            }
+            TreeNode lnode = lstack.pop(), rnode = rstack.pop();
+            if (lnode.val != rnode.val) {
+                return false;
+            }
+            if (lnode.left) {
+                lstack.push(lnode.left);
+            }
+            if (rnode.right) {
+                rstack.push(rnode.right);
+            }
+            if (lnode.right) {
+                rstack.push(lnode.right);
+            }
+            if (rnode.left) {
+                lstack.push(rnode.left);
+            }
+        }
+        return false;
     }
 }
