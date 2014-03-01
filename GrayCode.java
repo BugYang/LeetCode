0482c4ce54ff9adaa32857ac20f86d8bb92ef6c6
Zhiyuan-Yang <sjtu.yzy@gmail.com>
2014-03-01 18:33:24 +0800

    add brute force solution for graycode

---
+++ GrayCode.java
@@ -0,0 +1,41 @@
+public class Solution {
+    public boolean oneBitDiff(int a, int b) {
+        int c = a^b, cnt = 0;
+        while (c > 0) {
+            cnt += c & 1;
+            c = c >> 1;
+        }
+        return cnt == 1;
+    }
+
+    public ArrayList<Integer> grayCode_bruteforce(int n) {
+        int pow = 1;
+        for (int i = 0; i < n; i++) {
+            pow *= 2;
+        }
+
+        int[] seq = new int[pow];
+        for (int i = 0; i < pow; i++) {
+            seq[i] = i;
+        }
+
+        for (int i = 0; i < pow-1; i++) {
+            if (oneBitDiff(seq[i], seq[i+1])) {
+                continue;
+            }
+            for (int j = i+2; j < pow; j++) {
+                if (oneBitDiff(seq[i], seq[j])) {
+                    int t = seq[i+1];
+                    seq[i+1] = seq[j];
+                    seq[j] = t;
+                    break;
+                }
+            }
+        }
+
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        for (int i : seq) {
+            result.add(i);
+        }
+        return result;
+    }
