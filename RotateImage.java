1f304867ba31927d1eba9347c0a0dd00d6a850b2
Zhiyuan-Yang <sjtu.yzy@gmail.com>
2014-03-02 20:02:40 +0800

    refact code of rotate image problem

--- RotateImage.java
+++ RotateImage.java
@@ -6,12 +6,9 @@ public class Solution {
                 result[i][j] = matrix[i][j];
             }
         }
-        double origin = (matrix.length - 1.0) / 2;
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix.length; j++) {
-                int x = (int)(j + 0.5);
-                int y = (int)(2 * origin - i + 0.5);
-                matrix[x][y] = result[i][j];
+                matrix[j][matrix.length-1-i] = result[i][j];
             }
         }
     }
