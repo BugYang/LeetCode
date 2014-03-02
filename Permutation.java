5c2e4c74cf98b4e6a4120ab3c5d94385c7aa0be5
Zhiyuan-Yang <sjtu.yzy@gmail.com>
2014-03-02 18:36:25 +0800

    add solution for permutation problem

---
+++ Permutation.java
@@ -0,0 +1,30 @@
+public class Solution {
+    public ArrayList<ArrayList<Integer>> permute(int[] num) {
+        return permuteHelper(num, 0, num.length-1);
+    }
+
+    public ArrayList<ArrayList<Integer>> permuteHelper(int[] num, int begin, int end) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        if (begin > end) {
+            return result;
+        }
+        if (begin == end) {
+            ArrayList<Integer> p = new ArrayList<Integer>();
+            p.add(num[begin]);
+            result.add(p);
+            return result;
+        }
+
+        ArrayList <ArrayList<Integer>> subResult = permuteHelper(num, begin + 1, end);
+        for (ArrayList<Integer> list : subResult) {
+            for (int i = 0; i <= end - begin; i++) {
+                ArrayList<Integer> p = new ArrayList<Integer>();
+                p.addAll(list.subList(0, i));
+                p.add(num[begin]);
+                p.addAll(list.subList(i, end -begin));
+                result.add(p);
+            }
+        }
+        return result;
+    }
+}
