98c94dd459e49ba32f2823cd213266f234b64b78
Zhiyuan-Yang <sjtu.yzy@gmail.com>
2014-03-02 20:40:09 +0800

    add part of new solution for rotate image

--- RotateImage.java
+++ RotateImage.java
@@ -13,3 +13,20 @@ public class Solution {
         }
     }
 }
+
+public class Solution {
+    public void rotate(int[][] matrix) {
+        for (int i = 0; i < (matrix.length+1)/2; i++) {
+            for (int j = 0; j < (matrix.length+1)/2; j++) {
+                int x = i, y = j, cur = matrix[x][y];
+                for (int k = 0; k < 4; k++) {
+                    int t = matrix[y][matrix.length-1-x];
+                    matrix[y][matrix.length-1-x] = cur;
+                    cur = t;
+                    int x1 = y, y1 = matrix.length-1-x;
+                    x = x1; y = y1;
+                }
+            }
+        }
+    }
+}
