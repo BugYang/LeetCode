8f0e51b5c499144563e0cddecfd9fb9088f8dbc8
Yang Zhiyuan <sjtu.yzy@gmail.com>
2014-03-05 19:28:59 +0800

    refact code in unique path problem

--- UniquePath.java
+++ UniquePath.java
@@ -3,12 +3,6 @@ public class Solution {
         long result = 1, x = 2;
         for (int i = m; i <= m+n-2; i++) {
             long a = result, b = i;
-            while (a%x == 0 && x < n) {
-                a /= x++;
-            }
-            while (b%x == 0 && x < n) {
-                b /= x++;
-            }
             result = a * b;
             while (result%x == 0 && x < n) {
                 result /= x++;
