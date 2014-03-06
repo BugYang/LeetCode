c21928335a1610f5fdc063d8becfd1b8db5246d6
Yang Zhiyuan <sjtu.yzy@gmail.com>
2014-03-06 16:24:37 +0800

    fix last commit

--- SearchMatrix.java
+++ SearchMatrix.java
@@ -33,21 +33,5 @@ public class Solution {
         }
 
         return Arrays.binarySearch(matrix[begin], target) >= 0;
-    }    public boolean searchMatrix(int[][] matrix, int target) {
-        int m = matrix.length, n = matrix[0].length;
-        int begin = 0, end = m-1;
-        while (begin < end) {
-            int mid = (begin+end)/2;
-            if (matrix[mid][0] > target) {
-                end = mid - 1;
-            } else if (matrix[mid][0] <= target && matrix[mid][n-1] >= target) {
-                begin = mid;
-                break;
-            } else {
-                begin = mid + 1;
-            }
-        }
-
-        return Arrays.binarySearch(matrix[begin], target) >= 0;
-    }
+    }   
 }
