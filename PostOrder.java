26de45d3a8e4db2c3f7f033bbcf25fb081e1623a
Yang Zhiyuan <sjtu.yzy@gmail.com>
2014-03-06 11:06:39 +0800

    add 2-stack iter solution for post order traversal

--- PostOrder.java
+++ PostOrder.java
@@ -8,7 +8,7 @@
  * }
  */
 public class Solution {
-    public ArrayList<Integer> postorderTraversal(TreeNode root) {
+    public ArrayList<Integer> postorderTraversal_recur(TreeNode root) {
         ArrayList<Integer> result = new ArrayList<Integer>();
         if (root == null) {
             return result;
@@ -17,5 +17,32 @@ public class Solution {
         result.addAll(postorderTraversal(root.right));
         result.add(root.val);
         return result;
+    }
+
+    public ArrayList<Integer> postorderTraversal_iter_2stack(TreeNode root) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        Stack<TreeNode> extract = new Stack<TreeNode>();
+        Stack<TreeNode> traversal = new Stack<TreeNode>();
+
+        if (root != null) {
+            extract.push(root);
+        }
+        while (!extract.empty()) {
+            TreeNode cur = extract.pop();
+            if (cur.left != null) {
+                extract.push(cur.left);
+            }
+            if (cur.right != null) {
+                extract.push(cur.right);
+            }
+            traversal.push(cur);
+        }
+
+        while (!traversal.empty()) {
+            TreeNode cur = traversal.pop();
+            result.add(cur.val);
+        }
+        return result;
+
     }
 }
