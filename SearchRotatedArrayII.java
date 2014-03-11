854bf175f755ac37713bf3c78ec9838fb89d344e
Yang Zhiyuan <sjtu.yzy@gmail.com>
2014-03-11 10:51:08 +0800

    fix solution for search rotated array ii

--- SearchRotatedArrayII.java
+++ SearchRotatedArrayII.java
@@ -1,15 +1,6 @@
 public class Solution {
-    public int search_bf(int[] A, int target) {
-        for (int i = 0; i < A.length; i++) {
-            if (A[i] == target) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    public int search_bs(int[] A, int target) {
-        return searchHelper(A, 0, A.length-1, target);
+    public boolean search(int[] A, int target) {
+        return searchHelper(A, 0, A.length-1, target) != -1;
     }
 
     public int searchHelper(int[] A, int begin, int end, int target) {
@@ -17,12 +8,14 @@ public class Solution {
             int mid = (begin + end)/2;
             if (A[mid] == target) {
                 return mid;
-            } else if (A[begin] <= A[mid]) {
+            } else if (A[begin] < A[mid]) {
                 if (A[begin] <= target && target < A[mid]) {
                     end = mid;
                 } else {
                     begin = mid + 1;
                 }
+            } else if (A[begin] == A[mid]) {
+                begin += 1;
             } else {
                 if (A[mid] < target && target <= A[end]) {
                     begin = mid + 1;
