4efb08addcc2aabe51c20d67dbe0de4b41a24ffb
Zhiyuan-Yang <sjtu.yzy@gmail.com>
2014-03-11 08:06:18 +0800

    add binarySearch solution for search rotated array

--- SearchRotatedArray.java
+++ SearchRotatedArray.java
@@ -8,5 +8,29 @@ public class Solution {
         return -1;
     }
 
+    public int search_bs(int[] A, int target) {
+        return searchHelper(A, 0, A.length-1, target);
+    }
 
+    public int searchHelper(int[] A, int begin, int end, int target) {
+        while (begin < end) {
+            int mid = (begin + end)/2;
+            if (A[mid] == target) {
+                return mid;
+            } else if (A[mid] < target) {
+                if (A[begin] <= target && A[begin] > A[mid]) {
+                    end = mid;
+                } else {
+                    begin = mid + 1;
+                }
+            } else {
+                if (A[begin] > target && A[begin] <= A[mid]) {
+                    begin = mid + 1;
+                } else {
+                    end = mid;
+                }
+            }
+        }
+        return A[begin] == target ? begin : -1;
+    }
 }
