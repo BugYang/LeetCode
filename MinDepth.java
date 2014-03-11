16465a0b4887b19fe57244c52f878963cb8724bc
Yang Zhiyuan <sjtu.yzy@gmail.com>
2014-03-11 15:19:55 +0800

    add iterative solution for mindepth problem

--- MinDepth.java
+++ MinDepth.java
@@ -8,7 +8,7 @@
  * }
  */
 public class Solution {
-    public int minDepth(TreeNode root) {
+    public int minDepth_recur(TreeNode root) {
         if (root == null) {
             return 0;
         }
@@ -17,6 +17,35 @@ public class Solution {
             return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
         } else {
             return Math.max(minDepth(root.left), minDepth(root.right)) + 1;
+        }
+    }
+
+    public int minDepth_iter(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        int level = 1, cnt = 1;
+        queue.add(root);
+        while (true) {
+            int new_cnt = 0;
+            for (int i = 0; i < cnt; i++) {
+                TreeNode node = queue.poll();
+                if (node.left == null && node.right == null) {
+                    return level;
+                }
+                if (node.left != null) {
+                    new_cnt++;
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    new_cnt++;
+                    queue.add(node.right);
+                }
+            }
+            level++;
+            cnt = new_cnt;
         }
     }
 }
