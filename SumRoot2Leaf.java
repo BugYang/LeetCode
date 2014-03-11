0fcaf750e05c019cd8fff1c37adeb6a31944ed15
Yang Zhiyuan <sjtu.yzy@gmail.com>
2014-03-11 16:14:14 +0800

    add iterative solution for sum root to leaf

--- SumRoot2Leaf.java
+++ SumRoot2Leaf.java
@@ -36,7 +36,7 @@ public class Solution {
   *       * }
   *        */
 public class Solution {
-    public int sumNumbers(TreeNode root) {
+    public int sumNumbers_recur(TreeNode root) {
         if (root == null) {
             return 0;
         }
@@ -53,5 +53,41 @@ public class Solution {
             result += sumNumbersHelper(root.right, base);
         }
         return result == 0 ? base : result;
+    }
+
+    public int sumNumbers_iter(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        Queue<TreeNode> nodeQ = new LinkedList<TreeNode>();
+        Queue<Integer> intQ = new LinkedList<Integer>();
+        int result = 0, cnt = 1;
+        nodeQ.add(root);
+        intQ.add(0);
+
+        while (cnt != 0) {
+            int new_cnt = 0;
+            for (int i = 0; i < cnt; i++) {
+                TreeNode node = nodeQ.poll();
+                int base = intQ.poll();
+                if (node.left == null && node.right == null) {
+                    result += base * 10 + node.val;
+                }
+                if (node.left != null) {
+                    nodeQ.add(node.left);
+                    intQ.add(base * 10 + node.val);
+                    new_cnt++;
+                }
+                if (node.right != null) {
+                    nodeQ.add(node.right);
+                    intQ.add(base * 10 + node.val);
+                    new_cnt++;
+                }
+            }
+            cnt = new_cnt;
+        }
+
+        return result;
     }
 }
