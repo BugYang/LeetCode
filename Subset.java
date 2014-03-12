0ead1ea854af6d9b5a8555c539800a44c63b6bef
Yang Zhiyuan <sjtu.yzy@gmail.com>
2014-03-12 14:01:24 +0800

    add iterative solution for subset

--- Subset.java
+++ Subset.java
@@ -1,5 +1,5 @@
 public class Solution {
-    public ArrayList<ArrayList<Integer>> subsets(int[] S) {
+    public ArrayList<ArrayList<Integer>> subsets_recur(int[] S) {
         int[] SS = Arrays.copyOf(S, S.length);
         Arrays.sort(SS);
         return subsetsHelper(SS, SS.length-1);
@@ -18,6 +18,24 @@ public class Solution {
                 result.add(set);
             }
         }
+        return result;
+    }
+
+    public ArrayList<ArrayList<Integer>> subsets_iter(int[] S) {
+        int[] SS = Arrays.copyOf(S, S.length);
+        Arrays.sort(SS);
+
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        result.add(new ArrayList<Integer>());
+        for (int num : SS) {
+            int len = result.size();
+            for (int i = 0; i < len; i++) {
+                ArrayList<Integer> set = (ArrayList<Integer>)result.get(i).clone();
+                set.add(num);
+                result.add(set);
+            }
+        }
+
         return result;
     }
 }
