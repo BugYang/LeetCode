e1617969c6a19716c6990a357cda96ef1c603d1b
Zhiyuan Yang <sjtu.yzy@gmail.com>
2014-08-29 17:14:15 -0400

    add solution for zigzag conversion

--- ZigzagConversion.java
+++ ZigzagConversion.java
@@ -1,1 +1,23 @@
-aba500ad00cfc516e9908ffd04ee6a05ae4ecdd0
+public class Solution {
+    public String convert(String s, int nRows) {
+        if (nRows == 1)
+            return s;
+            
+        char[] str = s.toCharArray();
+        char[] result = new char[str.length];
+        int idx = 0, step;
+        
+        for (int i = 0; i < nRows; i++) {
+            step = 2 * (nRows - 1 - i);
+            int j = i;
+            while (j < str.length) {
+                if (step != 0)
+                    result[idx++] = str[j];
+                j += step;
+                step = 2 * (nRows - 1) - step;
+            }
+        }
+        
+        return new String(result);
+    }
+}
