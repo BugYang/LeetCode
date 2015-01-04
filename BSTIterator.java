fc1da4ed8ef128f95d0e817fe99393c9f9b114a9
Zhiyuan Yang <sjtu.yzy@gmail.com>
2015-01-04 13:34:49 -0500

    add solution for BST iterator

---
+++ BSTIterator.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+public class BSTIterator {
+    private Stack<TreeNode> stack = new Stack<TreeNode>();
+
+    public BSTIterator(TreeNode root) {
+        for (; root != null; root = root.left)
+            stack.push(root);
+    }
+
+    /** @return whether we have a next smallest number */
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+
+    /** @return the next smallest number */
+    public int next() {
+        TreeNode node = stack.pop();
+        int result = node.val;
+        
+        for (node = node.right; node != null; node = node.left)
+            stack.push(node);
+                
+        return result;
+    }
+}
+
+/**
+ * Your BSTIterator will be called like this:
+ * BSTIterator i = new BSTIterator(root);
+ * while (i.hasNext()) v[f()] = i.next();
+ */
